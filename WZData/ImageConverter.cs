3474ef85dfb0941bddf041c4d0330cd6558fb130
Inumedia <inumedia@inumedia.net>
2017-06-12 02:12:06 -0500

    Correcting image to base64 serialization not happening

--- WZData/ImageConverter.cs
+++ WZData/ImageConverter.cs
@@ -11,7 +11,7 @@ namespace WZData
     {
         public override bool CanConvert(Type objectType)
         {
-            return objectType == typeof(IImageBase);
+            return objectType == typeof(Image<Rgba32>);
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
