aaf435e96a04ff19905521c01e7a1e3cf08cab45
Andy <Inumedia@users.noreply.github.com>
2017-09-09 21:35:17 -0500

    Correcting resize being able to crash the server (rip)

--- maplestory.io/Extensions.cs
+++ maplestory.io/Extensions.cs
@@ -16,12 +16,15 @@ namespace maplestory.io
             {
                 string userResizeAmount = context.Query["resize"];
                 decimal resizeAmount = decimal.Parse(userResizeAmount);
-                img = (new Image<Rgba32>(img)).Resize(new ImageSharp.Processing.ResizeOptions()
+                if (resizeAmount != 1 && (img.Height * resizeAmount) < 50000 && (img.Width * resizeAmount) < 50000)
                 {
-                    Mode = ImageSharp.Processing.ResizeMode.Stretch,
-                    Sampler = new ImageSharp.Processing.NearestNeighborResampler(),
-                    Size = new SixLabors.Primitives.Size((int)(img.Width * resizeAmount), (int)(img.Height * resizeAmount))
-                });
+                    img = (new Image<Rgba32>(img)).Resize(new ImageSharp.Processing.ResizeOptions()
+                    {
+                        Mode = ImageSharp.Processing.ResizeMode.Stretch,
+                        Sampler = new ImageSharp.Processing.NearestNeighborResampler(),
+                        Size = new SixLabors.Primitives.Size((int)(img.Width * resizeAmount), (int)(img.Height * resizeAmount))
+                    });
+                }
             }
 
             using (MemoryStream mem = new MemoryStream())
