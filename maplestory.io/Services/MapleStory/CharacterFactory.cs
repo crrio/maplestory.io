99b3115a2d03dd95af99977cb0f61a337d9b9695
Andy <inumedia@inumedia.net>
2018-01-21 05:01:42 -0800

    Add FrameCounts to detailed character avatar

--- maplestory.io/Services/MapleStory/CharacterFactory.cs
+++ maplestory.io/Services/MapleStory/CharacterFactory.cs
@@ -73,7 +73,7 @@ namespace maplestory.io.Services.MapleStory
             return avatar;
         }
 
-        public Tuple<Image<Rgba32>, Dictionary<string, Point>> GetDetailedCharacter(int id, string animation = null, int frame = 0, bool showEars = false, bool showLefEars = false, int padding = 2, string name = null, float zoom = 1, bool flipX = false, RenderMode renderMode = RenderMode.Full,  params Tuple<int, string, float?>[] itemEntries)
+        public Tuple<Image<Rgba32>, Dictionary<string, Point>, Dictionary<string, int>> GetDetailedCharacter(int id, string animation = null, int frame = 0, bool showEars = false, bool showLefEars = false, int padding = 2, string name = null, float zoom = 1, bool flipX = false, RenderMode renderMode = RenderMode.Full,  params Tuple<int, string, float?>[] itemEntries)
         {
             CharacterAvatar avatar = getAvatar(id, animation, frame, showEars, showLefEars, padding, name, zoom, flipX, itemEntries);
             avatar.Mode = renderMode;
@@ -83,7 +83,7 @@ namespace maplestory.io.Services.MapleStory
         public IEnumerable<Tuple<Frame, Point, float?>> GetJsonCharacter(int id, string animation = null, int frame = 0, bool showEars = false, bool showLefEars = false, int padding = 2, string name = null, float zoom = 1, bool flipX = false, params Tuple<int, string, float?>[] itemEntries)
         {
             CharacterAvatar avatar = getAvatar(id, animation, frame, showEars, showLefEars, padding, name, zoom, flipX, itemEntries);
-            return avatar.GetFrameParts();
+            return avatar.GetFrameParts().Select(c => new Tuple<Frame, Point, float?>(c.Item1, c.Item2, c.Item3));
         }
 
         public Image<Rgba32> GetCharacter(int id, string animation = null, int frame = 0, bool showEars = false, bool showLefEars = false, int padding = 2, string name = null, float zoom = 1, bool flipX = false, RenderMode renderMode = RenderMode.Full, params Tuple<int, string, float?>[] itemEntries)
