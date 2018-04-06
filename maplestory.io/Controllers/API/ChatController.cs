e2ed505a3e7f0a86fa6a60fa793924db797f7f51
Andy <inumedia@inumedia.net>
2018-04-06 12:29:47 -0700

    Correcting chat bubbles not aligning properly all the time

--- maplestory.io/Controllers/API/ChatController.cs
+++ maplestory.io/Controllers/API/ChatController.cs
@@ -22,6 +22,8 @@ namespace maplestory.io.Controllers.API
         [HttpGet]
         public IActionResult GetChat([FromQuery]string ringIdsJoined, [FromQuery]string message)
         {
+            if (message.Length > 256) throw new InvalidOperationException("Too long of a message");
+
             int[] ringIds = ringIdsJoined.Split(',').Select(b => int.TryParse(b, out int d) ? (int?)d : null).Where(b => b.HasValue).Select(b => b.Value).ToArray();
             WZProperty rings = WZ.Resolve("Character/Ring");
             int chatBalloonID = ringIds.Select(b => rings.ResolveFor<int>($"{b.ToString("D8")}.img/info/chatBalloon")).Where(b => b.HasValue).Select(b => b.Value).FirstOrDefault();
@@ -94,18 +96,27 @@ namespace maplestory.io.Controllers.API
             string[] lines = message.Batch(15).Select(b => new string(b.ToArray())).ToArray();
             SizeF[] lineSizes = lines.Select(line => TextMeasurer.Measure(line, textOptions)).ToArray();
 
+            int leftPadding = new[] { nw.Width, w.Width, sw.Width }.Max();
+            int topPadding = new[] { ne.Height, n.Height, nw.Height }.Max();
+            int rightPadding = new[] { ne.Width, e.Width, se.Width }.Max();
+            int bottomPadding = new[] { se.Height, s.Height, sw.Height }.Max() + arrow.Height;
+
             int textWidth = (int)Math.Ceiling(lineSizes.Select(b => b.Width).Max());
-            int width = c.Width + e.Width + n.Width + s.Width + ne.Width + nw.Width + se.Width + sw.Width + textWidth;
-            int textHeight = (int)Math.Ceiling(lineSizes.Sum(b => b.Height)); ;
-            int height = c.Height + e.Height + n.Height + s.Height + ne.Height + nw.Height + se.Height + sw.Height + arrow.Height + textHeight;
-            Point center = new Point(width / 2, height / 2 - arrow.Height);
-            Point nwPos = new Point(center.X - textWidth / 2, center.Y - textHeight / 2);
-            Point nePos = new Point(center.X + textWidth / 2, center.Y - textHeight / 2);
-            Point swPos = new Point(center.X - textWidth / 2, center.Y + textHeight / 2);
-            swPos = new Point(swPos.X, swPos.Y + (swPos.Y % (sw.Height - swOrigin.Y)));
-            Point sePos = new Point(center.X + textWidth / 2, center.Y + textHeight / 2);
-            sePos = new Point(sePos.X, sePos.Y + (sePos.Y % (se.Height - seOrigin.Y)));
-            Point arrowPos = new Point(center.X, center.Y + textHeight + s.Height - sOrigin.Y);
+            textWidth = textWidth + (c.Width - (textWidth % c.Width));
+            if (textWidth < c.Width * 2) textWidth = c.Width * 2;
+            int width = leftPadding + rightPadding + textWidth;
+            int textHeight = (int)Math.Ceiling(lineSizes.Sum(b => b.Height));
+            int adjustTextY = (c.Height - (textHeight % c.Height));
+            textHeight = textHeight + adjustTextY;
+            if (textHeight < c.Height * 2) textHeight = c.Height * 2;
+            int height = topPadding + bottomPadding + textHeight;
+            Point center = new Point(width / 2, (height - arrow.Height) / 2);
+
+            Point nwPos = new Point((leftPadding - nw.Width) + nwOrigin.X, nwOrigin.Y);
+            Point nePos = new Point(neOrigin.X + width - rightPadding, neOrigin.Y);
+            Point swPos = new Point((leftPadding - sw.Width) + swOrigin.X, swOrigin.Y + height - bottomPadding);
+            Point sePos = new Point(seOrigin.X + width - rightPadding, seOrigin.Y + height - bottomPadding);
+            Point arrowPos = new Point(center.X, arrowOrigin.Y + height - arrow.Height);
             Image<Rgba32> result = new Image<Rgba32>(width, height);
 
             Tuple<Image<Rgba32>, Point, Point>[] corners = new Tuple<Image<Rgba32>, Point, Point>[] {
@@ -117,35 +128,35 @@ namespace maplestory.io.Controllers.API
 
             Tuple<Image<Rgba32>, Point, Point, Point>[] sides = new Tuple<Image<Rgba32>, Point, Point, Point>[]
             {
-                new Tuple<Image<Rgba32>, Point, Point, Point>(w, wOrigin, nwPos, swPos),
-                new Tuple<Image<Rgba32>, Point, Point, Point>(e, eOrigin, nePos, sePos),
-                new Tuple<Image<Rgba32>, Point, Point, Point>(n, nOrigin, nwPos, nePos),
-                new Tuple<Image<Rgba32>, Point, Point, Point>(s, sOrigin, swPos, sePos),
+                new Tuple<Image<Rgba32>, Point, Point, Point>(w, wOrigin, new Point(leftPadding, nw.Height), new Point(leftPadding, swPos.Y - swOrigin.Y)),
+                new Tuple<Image<Rgba32>, Point, Point, Point>(e, eOrigin, new Point(width - rightPadding, ne.Height), new Point(width - rightPadding, sePos.Y - seOrigin.Y)),
+                new Tuple<Image<Rgba32>, Point, Point, Point>(n, nOrigin, new Point(nw.Width, topPadding), new Point(width - rightPadding, topPadding)),
+                new Tuple<Image<Rgba32>, Point, Point, Point>(s, sOrigin, new Point((swPos.X + sw.Width - swOrigin.X), height - bottomPadding), new Point(width - rightPadding, height - bottomPadding))
             };
 
             result.Mutate(image =>
             {
-                for (int x = nwPos.X - cOrigin.X; x < nePos.X; x += c.Width)
-                    for (int y = nwPos.Y - cOrigin.X; y < swPos.Y; y += c.Height)
+                for (int x = leftPadding; x < textWidth + leftPadding; x += c.Width)
+                    for (int y = topPadding; y < textHeight + topPadding; y += c.Height)
                         image.DrawImage(c, 1, new Size(c.Width, c.Height), new Point(x, y));
                 foreach (var side in sides)
                 {
-                    for (int x = side.Item3.X - side.Item2.X; x <= side.Item4.X; x += (side.Item1.Width))
+                    for (int x = side.Item3.X; x <= side.Item4.X; x += (side.Item1.Width))
                     {
-                        for (int y = side.Item3.Y - side.Item2.Y; y <= side.Item4.Y; y += (side.Item1.Height))
+                        for (int y = side.Item3.Y; y <= side.Item4.Y; y += (side.Item1.Height))
                         {
-                            image.DrawImage(side.Item1, 1, new Size(side.Item1.Width, side.Item1.Height), new Point(x, y));
-                            if (side.Item3.Y == side.Item4.Y) break;
+                            image.DrawImage(side.Item1, 1, new Size(side.Item1.Width, side.Item1.Height), new Point(x - side.Item2.X, y - side.Item2.Y));
+                            if (side.Item3.Y == side.Item4.Y || (y + side.Item1.Height >= side.Item4.Y)) break;
                         }
-                        if (side.Item3.X == side.Item4.X) break;
+                        if (side.Item3.X == side.Item4.X || (x + side.Item1.Width >= side.Item4.X)) break;
                     }
                 }
-                foreach (var corner in corners) image.DrawImage(corner.Item1, 1, new Size(corner.Item1.Width, corner.Item1.Height), new Point(corner.Item3.X - corner.Item2.X, corner.Item3.Y - corner.Item2.Y));
+                foreach (var corner in corners) image.DrawImage(corner.Item1, PixelBlenderMode.Src, 1, new Size(corner.Item1.Width, corner.Item1.Height), new Point(corner.Item3.X - corner.Item2.X, corner.Item3.Y - corner.Item2.Y));
                 for (int i = 0; i < lines.Length; ++i)
                 {
                     int x = center.X - ((int)lineSizes[i].Width / 2);
-                    int y = (int)lineSizes.Take(i).Select(b => b.Height).DefaultIfEmpty(0).Sum(b => b) + (center.Y - (textHeight / 2));
-                    image.DrawText(lines[i], font, nameColor, new Point(x, y));
+                    int y = (int)lineSizes.Take(i).Select(b => b.Height).DefaultIfEmpty(0).Sum(b => b) + ((height - arrow.Height) - textHeight) / 2;
+                    image.DrawText(lines[i], font, nameColor, new Point(x, y + adjustTextY / 2));
                 }
 
                 image.DrawImage(arrow, 1, new Size(arrow.Width, arrow.Height), new Point(center.X - arrowOrigin.X, swPos.Y - arrowOrigin.Y));
