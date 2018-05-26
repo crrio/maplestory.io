4d26e5ef363fac047f9b2bfdb1deae3d0fd3e086
Andy <Inumedia@inumedia.net>
2018-05-26 02:53:33 -0500

    Removing unnecessary reference to RethinkDb

--- maplestory.io/Startup.cs
+++ maplestory.io/Startup.cs
@@ -112,7 +112,7 @@ namespace maplestory.io
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IRethinkDbConnectionFactory connectionFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 
